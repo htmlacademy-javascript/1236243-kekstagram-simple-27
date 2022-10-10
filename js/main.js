e333236a61a57a96dab8d6fb6f77175604939ac3
Iakovlev-Dev <81575417+Iakovlev-Dev@users.noreply.github.com>
2022-10-10 20:41:39 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-let getRandom = function (min, max) {
+let getRandomNumber = function (min, max) {
     if (min === max || min > max) {
         return NaN
     } else {
@@ -8,10 +8,10 @@ let getRandom = function (min, max) {
             if(min >= 0 && max > 0) {
                 min = Math.ceil(min);
                 max = Math.floor(max);
-                randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-                return console.log('Результат: ' + randomNumber + ' из диапазона "от ' + min + ' до ' + max + '"')
+                let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+                return randomNumber
             } else {
-                return console.log('Введите числа больше 0')
+                return NaN
             }
         } else {
             return NaN
