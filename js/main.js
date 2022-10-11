ebfe7fd8d8f9bcb5fca5d4183e819c9a5a6df770
Игорь Яковлев <iakovlev.igor.dev@gmail.com>
2022-10-11 21:27:41 +0300

    Исправил ошибки

--- js/main.js
+++ js/main.js
@@ -1,28 +1,31 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 const GET_RANDOM_NUMBER = function (min, max) {
-    if (min === max || min > max) {
-        return NaN
+  if (min === max || min > max) {
+    return NaN;
+  } else {
+    if (typeof min === 'number' && typeof max === 'number') {
+      if(min >= 0 && max > 0) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        return randomNumber;
+      } else {
+        return NaN;
+      }
     } else {
-        if (typeof min === 'number' && typeof max === 'number') {    
-            if(min >= 0 && max > 0) {
-                min = Math.ceil(min);
-                max = Math.floor(max);
-                let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-                return randomNumber
-            } else {
-                return NaN
-            }
-        } else {
-            return NaN
-        }
+      return NaN;
     }
-}
+  }
+};
 
 const COMMENT_LENGTH = function (comment, maxLength) {
-    if (comment.length <= maxLength) {
-        return true
-    } else {
-        return false
-    }
-}
+  if (comment.length <= maxLength) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+GET_RANDOM_NUMBER();
+COMMENT_LENGTH();
