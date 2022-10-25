d9351fe5c70869f4e6c724261e0e94446cfae2bd
Keks <keks@htmlacademy.ru>
2022-10-25 11:35:55 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/data.js
@@ -0,0 +1,44 @@
+import {createDescriptionFoto, GET_RANDOM_NUMBER} from './util.js';
+
+const FOTOS = 25;
+
+const DESCRIPTION_ADVERB = [
+  'Невероятно',
+  'Восхитительно',
+  'Классно',
+  'Замечательно',
+  'Необычно',
+  'Странно'
+];
+
+const DESCRIPTION_VERB = [
+  'посидели',
+  'отдохнули',
+  'встретились',
+  'погуляли',
+  'поиграли',
+  'покатались',
+];
+
+
+const getFotos = function () {
+  const arrayDescription = [];
+  while (arrayDescription.length < FOTOS) {
+    const index = GET_RANDOM_NUMBER(1, 25);
+    let found = false;
+    for (let i = 0; i < arrayDescription.length; i++) {
+      if (arrayDescription[i].id === index) {
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      arrayDescription[arrayDescription.length] = createDescriptionFoto(index);
+    }
+  }
+  const SORTED = [...arrayDescription].sort((a,b) => a.id - b.id);
+  return SORTED;
+};
+
+export {getFotos,DESCRIPTION_ADVERB, DESCRIPTION_VERB};
+
