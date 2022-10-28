ca5a750c5107a433e5e2835e360f67c7e7aa21c8
Игорь Яковлев <iakovlev.igor.dev@gmail.com>
2022-10-28 15:53:06 +0300

    добавил фото, пофиксил

--- js/main.js
+++ js/main.js
@@ -1,2 +1,4 @@
-import {PICTURES} from './miniatures.js';
+import { PICTURES, PICTURES_FRAGMENT } from './miniatures.js';
+
+PICTURES.append(PICTURES_FRAGMENT);
 
--- js/miniatures.js
+++ js/miniatures.js
@@ -1,22 +1,21 @@
 import {getFotos} from './data.js';
 
-const PICTURES = document.querySelector('.pictures')
+const PICTURES = document.querySelector('.pictures');
 const PICTURE_TEMPLATE = document.querySelector('#picture')
-    .content
-    .querySelector('.picture')
+  .content
+  .querySelector('.picture');
 
-const GET_FOTOS = getFotos()
+const GET_FOTOS = getFotos();
 
-const PICTURES_FRAGMENT = document.createDocumentFragment()
+const PICTURES_FRAGMENT = document.createDocumentFragment();
 
 GET_FOTOS.forEach(({url, likes, comments}) => {
-    const FOTO_ELEMENT = PICTURE_TEMPLATE.cloneNode(true)
-    FOTO_ELEMENT.querySelector('img').src = url
-    FOTO_ELEMENT.querySelector('.picture__comments').textContent = comments
-    FOTO_ELEMENT.querySelector('.picture__likes').textContent = likes
-    PICTURES_FRAGMENT.appendChild(FOTO_ELEMENT)
-})
+  const FOTO_ELEMENT = PICTURE_TEMPLATE.cloneNode(true);
+  FOTO_ELEMENT.querySelector('img').src = url;
+  FOTO_ELEMENT.querySelector('.picture__comments').textContent = comments;
+  FOTO_ELEMENT.querySelector('.picture__likes').textContent = likes;
+  PICTURES_FRAGMENT.appendChild(FOTO_ELEMENT);
+});
 
-PICTURES.append(PICTURES_FRAGMENT)
 
-export { PICTURES }
+export { PICTURES, PICTURES_FRAGMENT };
