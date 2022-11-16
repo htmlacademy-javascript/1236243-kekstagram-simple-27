e89269c0b5f0f32f4de0109bd36a057eee537441
Keks <keks@htmlacademy.ru>
2022-11-16 11:04:13 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/message.js
@@ -0,0 +1,16 @@
+const templateErrorElement = document.querySelector('#error').content.querySelector('.error');
+const error = templateErrorElement.cloneNode(true);
+
+const templateSuccessElement = document.querySelector('#success').content.querySelector('.success');
+const success = templateSuccessElement.cloneNode(true);
+
+
+const showError = function () {
+  document.body.append(error);
+};
+
+const showSuccess = function () {
+  document.body.append(success);
+};
+
+export {showError, showSuccess};
