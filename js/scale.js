a7cf7244a2e6edde2f57572dd4c24c1b0ba48922
Игорь Яковлев <iakovlev.igor.dev@gmail.com>
2022-11-19 18:54:10 +0300

    9.1 Изменение масштаба

---
+++ js/scale.js
@@ -0,0 +1,39 @@
+import {formIMG, imgPreview} from './form.js'
+
+const valueScale = formIMG.querySelector('.scale__control--value');
+const buttonSmaller = formIMG.querySelector('.scale__control--smaller');
+const buttonBigger = formIMG.querySelector('.scale__control--bigger');
+
+const STEP = 25;
+const MIN_SCALE = 0;
+const MAX_STEP = 100;
+
+
+const changeScaleMin = function () {
+  buttonSmaller.addEventListener('click', () => {
+    const currentValue = Number.parseInt(valueScale.value, 10);
+    const newValue = currentValue - STEP;
+    valueScale.value = `${newValue}%`;
+    if (newValue <= MIN_SCALE) {
+      valueScale.value = `${MIN_SCALE}%`;
+    }
+    imgPreview.style.transform = `scale(${Number.parseInt(valueScale.value, 10) / 100})`;
+  });
+
+};
+
+const changeScaleMax = function () {
+  buttonBigger.addEventListener('click', () => {
+    const currentValue = Number.parseInt(valueScale.value, 10);
+    const newValue = currentValue + STEP;
+    valueScale.value = `${newValue}%`;
+    if (newValue >= MAX_STEP) {
+      valueScale.value = `${MAX_STEP}%`;
+    }
+    imgPreview.style.transform = `scale(${Number.parseInt(valueScale.value, 10) / 100})`;
+  });
+
+};
+
+export {changeScaleMin, changeScaleMax}
+
